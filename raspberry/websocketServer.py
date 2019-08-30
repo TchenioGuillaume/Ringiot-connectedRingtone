c0aef7be424176005f3f298e517b5c355e59d723
ValentinArnould <32564448+ValentinArnould@users.noreply.github.com>
2019-08-30 10:08:46 +0200

    Update websocketServer.py
    Websocket amélioré

--- raspberry/websocketServer.py
+++ raspberry/websocketServer.py
@@ -1,3 +1,5 @@
+# boucle avec un sleep et un switch case
+
 import asyncio
 import datetime
 import time
@@ -7,55 +9,67 @@ import websockets
 global connected
 connected = set()
 
-condition =  set()
+#websocket relance une pub_sub asynchrone a chaque appel
      
 async def pub_sub(websocket, path):
     global connected
 
-    if path == '/newConnection' : #quelqu'un se connecte
-        connected.add(websocket)
-        print("READER "+str(websocket.remote_address)+"    connected")
-        print(len(connected))
-        await asyncio.wait([websocket.send('connected')])
-        while True:
-            await asyncio.sleep(100)
+    connected.add(websocket)
+    print("READER "+str(websocket.remote_address)+"    connected")
+    print(len(connected))
+    await asyncio.wait([websocket.send('connected')])
+    while True:
+        try:
+            respo = await websocket.recv()
 
+            if respo == "notification" :
+                still_connected = set()
+                for ws in connected :
+                    if ws.open:
+                        still_connected.add(ws)
+                        await asyncio.wait([ws.send('notifDuRasp')])
+                    else:
+                        print("READER "+str(ws.remote_address)+" disconnected")
+                connected=still_connected
+                await asyncio.wait([websocket.send('notification sent')])
 
-    elif path == '/notification' : #trigger quelqu'un devant la porte
-        #envoyer la notification aux utilisateurs connectés
-        still_connected = set()
-        for ws in connected :
-            if ws.open:
-                still_connected.add(ws)
-                await asyncio.wait([ws.send('notifDuRasp')])
-            else:
-                print("READER "+str(ws.remote_address)+" disconnected")
-        connected=still_connected
+            elif respo == "refresh" :
+                still_connected = set()
+                for ws in connected :
+                    if ws.open:
+                        still_connected.add(ws)
+                        #await asyncio.wait([ws.send('notifDuRasp')])
+                    else:
+                        print("READER "+str(ws.remote_address)+" disconnected")
+                connected=still_connected
+                await asyncio.wait([websocket.send('list refreshed : ' +str(len(connected)) +' users online')])
 
-    elif path == '/refresh' : #pour rafraichir la liste d'utilisateurs toutes les tant de minutes
-        still_connected = set()
-        for ws in connected :
-            if ws.open:
-                still_connected.add(ws)
-                #await asyncio.wait([ws.send(data)])
-            else:
-                print("READER "+str(ws.remote_address)+" disconnected")
-        connected=still_connected
+            elif respo == "accessGranted" :
+                #allumer la led
+                still_connected = set()
+                for ws in connected :
+                    if ws.open:
+                        still_connected.add(ws)
+                        await asyncio.wait([ws.send('access Granted')])
 
-    elif path == '/accessGranted' : #allume la led par signal d'un utilisateur
-        #peut être aussi envoyer la notif granted aux autres utilisateurs
+                    else:
+                        print("READER "+str(ws.remote_address)+" disconnected")
+                connected=still_connected
+                await asyncio.wait([websocket.send('done')])
 
-        #allumer la led
+            else :
+                await asyncio.wait([websocket.send('invalid call')])
 
-        still_connected = set()
-        for ws in connected :
-            if ws.open:
-                still_connected.add(ws)
-                await asyncio.wait([ws.send('accesAutorise')])
-            else:
-                print("READER "+str(ws.remote_address)+" disconnected")
-        connected=still_connected
+            #await asyncio.wait([websocket.send('ok')])
+            #print(respo)
+            #await asyncio.sleep(100)
+        except:
+            print("session closed")
+            break
              
-start_serve = websockets.serve(pub_sub, '192.168.43.215', 5678)
+start_serve = websockets.serve(pub_sub, '172.20.10.2', 5678) #changer l'ip pour correspondre à l'ip de la raspberry
+                                                             #ne pas oublier d'ouvrir les ports sur l'host de connexion
+ 
 asyncio.get_event_loop().run_until_complete(start_serve)
+
 asyncio.get_event_loop().run_forever()
